717081277aabb488808c562cc2220318714a4869
rpalaparthi pscu.com <rpalaparthi@pscu.com>
2020-05-08 00:17:49 -0400

    adding oder moq data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[]=[
+    {
+      id: 1, customer:
+        { id: 1, name: '', state: 'co', email: 'none@gmail.com' },
+      total: 203, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 1)
+    },
+    {
+      id: 2, customer:
+        { id: 1, name: '', state: 'co', email: 'none@gmail.com' },
+      total: 203, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 1)
+    },
+    {
+      id: 3, customer:
+        { id: 1, name: '', state: 'co', email: 'none@gmail.com' },
+      total: 203, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 1)
+    },
+    {
+      id: 4, customer:
+        { id: 1, name: '', state: 'co', email: 'none@gmail.com' },
+      total: 203, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 1)
+    },
+    {
+      id: 5, customer:
+        { id: 1, name: '', state: 'co', email: 'none@gmail.com' },
+      total: 203, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 1)
+    }
+
+  ];
 
   ngOnInit(): void {
   }
