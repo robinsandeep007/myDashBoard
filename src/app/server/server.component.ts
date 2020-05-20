c94d1d8fd5e3e6cc6e8c84a7484de610ee538986
rpalaparthi pscu.com <rpalaparthi@pscu.com>
2020-05-20 00:27:40 -0400

    updated the card color

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Server } from '../shared/server';
 
 @Component({
   selector: 'app-server',
@@ -9,9 +10,32 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
-  @Input() serverInput;
+  color: string;
+  buttonText: string;
+
+  @Input() serverInput : Server;
 
   ngOnInit(): void {
+
+    this.setServerAction(this.serverInput.isOnline);
+  }
+
+  setServerAction(isOnline: boolean){
+   if(isOnline){
+   this.serverInput.isOnline = true;
+   this.color='#66bb6a';
+   this.buttonText ='Shutdown';
+   } else {
+     this.serverInput.isOnline = false;
+     this.color='#FF6B6B';
+     this.buttonText ='Start';
+   }
+  }
+
+  toggleStatus(onlineStatus: boolean)
+  {
+    console.log(this.serverInput.name, ':', onlineStatus);
+    this.setServerAction(!onlineStatus);
   }
 
 }
