e0d37bd30c58c7dc3ee6d26cdf41db6ee12f825e
rpalaparthi pscu.com <rpalaparthi@pscu.com>
2020-05-19 23:58:41 -0400

    d10

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
 
   constructor() { }
+
+  @Input() serverInput;
 
   ngOnInit(): void {
   }
